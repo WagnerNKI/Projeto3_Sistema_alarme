0b8776c9b5d8b2647f2d0d8031f7767004636f95
INFO\32658368860 <wagnwnki@gmail.com>
2017-11-23 21:11:33 -0200

    infravermelho + leds + teclado

--- Sensor___teclado/Sensor___teclado.ino
+++ Sensor___teclado/Sensor___teclado.ino
@@ -1,21 +1,23 @@
-#include <Ultrasonic.h>
 #include <Keypad.h>
 
-
-Ultrasonic ultrasonic(9, 8);
 bool digitandoSenha = false;
 String senhaDigitada;
 String senha = "1234";
-long timeDoorOpen;
+long timeDoorOpen = 0;
 int buzz = A1;
+int ledRed = A3;
+int ledYellow = A4;
+int ledGreen = A5;
+String senhaAtiva = "0";
+int infrared = 8;
 
 const byte ROWS = 4; //four rows
 const byte COLS = 3; //three columns
 char keys[ROWS][COLS] = {
-  {'1','2','3'},
-  {'4','5','6'},
-  {'7','8','9'},
-  {'*','0','#'}
+  {'1', '2', '3'},
+  {'4', '5', '6'},
+  {'7', '8', '9'},
+  {'*', '0', '#'}
 };
 
 byte rowPins[ROWS] = {5, 4, 3, A0}; //connect to the row pinouts of the keypad
@@ -23,56 +25,116 @@ byte colPins[COLS] = {A2, 7, 6}; //connect to the column pinouts of the keypad
 
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
-void setup(){
+void apagaAcendeLed(int ledAcesso, int ledApagado1, int ledApagado2) {
+  digitalWrite (ledAcesso, HIGH);
+  digitalWrite (ledApagado1, LOW);
+  digitalWrite (ledApagado2, LOW);
+}
+
+void setup() {
   Serial.begin(9600);
   pinMode(A2, INPUT);
   pinMode(A0, INPUT);
   pinMode(A1, OUTPUT);
+  pinMode(ledRed, OUTPUT);
+  pinMode(ledYellow, OUTPUT);
+  pinMode(ledGreen, OUTPUT);
+  pinMode(infrared, INPUT);
 }
+
+void loop() {
+
+  char key = keypad.getKey();
+  bool alarmeAtivado = false;
   
-void loop(){
-  int distancia = ultrasonic.distanceRead();
+  if (key) {
+    if (key == '#') {
+      Serial.println("Finalizou senha");
+      digitandoSenha = false;
 
-  if (distancia <= 5){
-    Serial.println("Favor digitar a senha em ate 10 segundos");
-    long now = millis();
-    bool senhaCerta = false;
+      if (senhaDigitada == senhaAtiva) {
+        Serial.println ("Alarme ativado");
+        alarmeAtivado = true;
+        apagaAcendeLed(ledYellow, ledGreen, ledRed);
+        delay(1000);
+      }
+      else {
+        Serial.println ("Senha incorreta");
+      }
 
-    while(senhaCerta == false){
-    if (now - timeDoorOpen > 10000){
-      timeDoorOpen = now;
-      tone(buzz, 500);
-      delay (1000);
-      noTone(buzz);
     }
-    char key = keypad.getKey();
-  
-    if (key){
-      if (key == '#'){
-        Serial.println("Finalizou senha");
-        digitandoSenha = false;
-        if (senhaDigitada == senha){
-          Serial.println ("Alarme desativado");
-          senhaCerta = true;
-          break;
-        }
-        else{
-          Serial.println ("Senha incorreta");
-        }
+
+    if (digitandoSenha) {
+      senhaDigitada += key;
+      Serial.println (senhaDigitada);
+    }
+
+    if (key == '*') {
+      senhaDigitada = "";
+      Serial.println("Iniciando digitação da senha...");
+      digitandoSenha = true;
+    }
+  }
+
+    while (alarmeAtivado == true) {
+    int doorOpenClosed = digitalRead(infrared);
+    
+    if (doorOpenClosed == 1) {
+      Serial.println("Favor digitar a senha em ate 10 segundos");
+      bool senhaCerta = false;
+      timeDoorOpen = millis();
+
+      while (senhaCerta == false) {
+        long now = millis();
+        Serial.println(now - timeDoorOpen);
         
-    }
-    
-      if (digitandoSenha){
-        senhaDigitada += key;
-        Serial.println (senhaDigitada);
-    }
-    
-      if(key == '*'){
-        senhaDigitada = "";
-        Serial.println("Iniciando digitação da senha...");
-        digitandoSenha = true;
+        char key = keypad.getKey();
+
+        if (key) {
+          if (key == '#') {
+            Serial.println("Finalizou senha");
+            digitandoSenha = false;
+            if (senhaDigitada == senha) {
+              Serial.println ("Alarme desativado");
+              senhaCerta = true;
+              timeDoorOpen = now;
+              apagaAcendeLed(ledGreen, ledRed, ledYellow);
+              alarmeAtivado = false;
             }
+            else {
+              Serial.println ("Senha incorreta");
+              apagaAcendeLed(ledRed, ledGreen, ledYellow);
+              tone(buzz, 500);
+              delay (1000);
+              noTone(buzz);
+            }
+
+          }
+
+          if (digitandoSenha) {
+            senhaDigitada += key;
+            Serial.println (senhaDigitada);
+          }
+
+          if (key == '*') {
+            senhaDigitada = "";
+            Serial.println("Iniciando digitação da senha...");
+            digitandoSenha = true;
+          }
+        }
+
+        if (now - timeDoorOpen > 20000) {
+          timeDoorOpen = now;
+          apagaAcendeLed(ledRed, ledGreen, ledYellow);
+          tone(buzz, 1000);
+          delay (1000);
+          noTone(buzz);
+        }
+
+      }
+
     }
   }
 }
-}
+
+
